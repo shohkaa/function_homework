5afa12f71e09f2f7da7c51722c11feb7e862a852
Baxodir <baxodirochildiyev55@gmail.com>
2024-02-16 14:42:54 +0500

    add second homework: work2

---
+++ work2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 2.
+// Задайте массив заполненный случайными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// Пример
+// [344 452 341 125 ] => 2
+
+
+int[] GetArrayRndInt(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(100, 1000);   
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void CalculateEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.Write($" => количество чётных чисел в массиве {count}");
+}
+
+Console.Write("Задайте размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] getArray = GetArrayRndInt(size);
+PrintArray(getArray);
+CalculateEvenNumbers(getArray);
